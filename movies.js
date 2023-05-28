bea0b512abd15641389a78cd4919894961602fb7
Callum Baldwin <53581256+Ladish-Ark@users.noreply.github.com>
2023-05-28 06:09:21 +0100

    Combined js files for Movie Page
    Put all the js files into one and also added a table function so there are multiple pages that just one long one with all the movies showing

---
+++ movies.js
@@ -0,0 +1,199 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const table = document.getElementById("movie-table");
+  const tbody = document.getElementById("movies");
+
+  let moviesData = []; // Variable to store the movie data
+  let currentPage = 1; // Current page number
+  const moviesPerPage = 20; // Number of movies to display per page
+
+  fetchMoviesData()
+    .then((data) => {
+      moviesData = data;
+      renderMovies(currentPage);
+      renderPagination();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+
+
+
+    
+  function fetchMoviesData() {
+    return new Promise((resolve, reject) => {
+      fetch("/Movie-Data/")
+        .then((response) => response.text())
+        .then((html) => {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(html, "text/html");
+
+          const fileLinks = Array.from(doc.querySelectorAll("a")).map(
+            (a) => a.href
+          );
+
+          const csvUrl = fileLinks
+            .filter((link) => link.endsWith(".csv"))
+            .sort()
+            .reverse()[0];
+
+          fetch(csvUrl)
+            .then((response) => response.text())
+            .then((data) => {
+              const parsedData = Papa.parse(data, {
+                header: true,
+                dynamicTyping: true,
+                skipEmptyLines: true,
+              }).data;
+
+              const processedData = parsedData.map((movie) => ({
+                date: movie["Date"],
+                title: movie["Name"],
+                year: movie["Year"],
+                rating: movie["Rating"],
+                link: movie["Letterboxd URI"],
+              }));
+
+              resolve(processedData);
+            })
+            .catch((error) => reject(error));
+        })
+        .catch((error) => reject(error));
+    });
+  }
+
+
+
+
+  function renderMovies(page) {
+    const startIndex = (page - 1) * moviesPerPage;
+    const endIndex = startIndex + moviesPerPage;
+    const moviesToShow = moviesData
+      .slice()
+      .sort((a, b) => new Date(b.date) - new Date(a.date)) // Sort movies by date, newest first
+      .slice(startIndex, endIndex);
+
+    tbody.innerHTML = ""; // Clear existing table rows
+
+    moviesToShow.forEach((movie) => {
+      const row = document.createElement("tr");
+      const dateCell = document.createElement("td");
+      const titleCell = document.createElement("td");
+      const yearCell = document.createElement("td");
+      const ratingCell = document.createElement("td");
+      const linkCell = document.createElement("td");
+      const link = document.createElement("a");
+
+      dateCell.textContent = movie.date;
+      titleCell.textContent = movie.title;
+      yearCell.textContent = movie.year;
+      ratingCell.textContent = movie.rating;
+
+      link.href = movie.link;
+      link.textContent = movie.link;
+      linkCell.appendChild(link);
+
+      row.appendChild(dateCell);
+      row.appendChild(titleCell);
+      row.appendChild(yearCell);
+      row.appendChild(ratingCell);
+      row.appendChild(linkCell);
+
+      tbody.appendChild(row);
+    });
+  }
+
+
+
+
+
+  function renderPagination() {
+    const totalPages = Math.ceil(moviesData.length / moviesPerPage);
+    const paginationContainer = document.getElementById("pagination");
+
+    paginationContainer.innerHTML = ""; // Clear existing pagination
+
+    const previousButton = document.createElement("a");
+    previousButton.href = "#";
+    previousButton.textContent = "Previous";
+    previousButton.addEventListener("click", function () {
+      if (currentPage > 1) {
+        currentPage--;
+        renderMovies(currentPage);
+        renderPagination();
+      }
+    });
+
+    const nextButton = document.createElement("a");
+    nextButton.href = "#";
+    nextButton.textContent = "Next";
+    nextButton.addEventListener("click", function () {
+      if (currentPage < totalPages) {
+        currentPage++;
+        renderMovies(currentPage);
+        renderPagination();
+      }
+    });
+
+    paginationContainer.appendChild(previousButton);
+
+    for (let i = 1; i <= totalPages; i++) {
+      const pageLink = document.createElement("a");
+      pageLink.href = "#";
+      pageLink.textContent = i;
+
+      if (i === currentPage) {
+        pageLink.classList.add("active");
+      }
+
+      pageLink.addEventListener("click", function () {
+        currentPage = i;
+        renderMovies(currentPage);
+        renderPagination();
+      });
+
+      paginationContainer.appendChild(pageLink);
+    }
+
+    paginationContainer.appendChild(nextButton);
+  }
+
+  const inputTitle = document.getElementById("search-title");
+  const inputDate = document.getElementById("search-date");
+  const inputRating = document.getElementById("search-rating");
+
+  inputTitle.addEventListener("input", searchMovies);
+  inputDate.addEventListener("input", searchMovies);
+  inputRating.addEventListener("input", searchMovies);
+
+  function searchMovies() {
+    const filterTitle = inputTitle.value.toUpperCase();
+    const filterDate = inputDate.value.toUpperCase();
+    const filterRating = inputRating.value.toUpperCase();
+
+    const rows = tbody.getElementsByTagName("tr");
+
+    for (let i = 0; i < rows.length; i++) {
+      const row = rows[i];
+      const titleCell = row.getElementsByTagName("td")[1];
+      const dateCell = row.getElementsByTagName("td")[0];
+      const ratingCell = row.getElementsByTagName("td")[3];
+
+      if (titleCell && dateCell && ratingCell) {
+        const titleMatch = titleCell.textContent || titleCell.innerText;
+        const dateMatch = dateCell.textContent || dateCell.innerText;
+        const ratingMatch = ratingCell.textContent || ratingCell.innerText;
+
+        if (
+          titleMatch.toUpperCase().indexOf(filterTitle) > -1 &&
+          dateMatch.toUpperCase().indexOf(filterDate) > -1 &&
+          ratingMatch.toUpperCase().indexOf(filterRating) > -1
+        ) {
+          row.style.display = "";
+        } else {
+          row.style.display = "none";
+        }
+      }
+    }
+  }
+});
